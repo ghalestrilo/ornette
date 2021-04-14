b087319559edb3506c49ffac4a83348f750b6a91
ghalestrilo <ghalestrilo@gmail.com>
2021-04-13 23:25:56 -0300

    :construction: create batchclient

--- scripts/batch.py
+++ scripts/batch.py
@@ -13,14 +13,43 @@ parser.add_argument('--state',      type=str,  default="0",         help='the in
 parser.add_argument("--playback",   type=bool, default=True,        help="Use supercollider for sound playback")
 parser.add_argument("--batch-mode", type=bool, default=False,       help="Use supercollider for sound playback")
 
-parser.add_argument('--repetitions',type=int,  default=256,         help='maximum buffer length')
-parser.add_argument('--experiment', type=str, default='all',        choices=['all', 'guess'], help='Which experiment to run')
+parser.add_argument('--iterations', type=int,  default=10,          help='Number of times to run the experiment')
+parser.add_argument('--experiment', type=str,  default='all',       choices=['all', 'guess'], help='Which experiment to run')
 
 args = parser.parse_args()
-client = udp_client.SimpleUDPClient(args.ip, args.port)
 
+
+
+
+class BatchClient(udp_client.SimpleUDPClient):
+  def __init__(self,ip,port):
+    udp_client.SimpleUDPClient.__init__(self,ip, port)
+
+  def start(self):
+    self.send_message('/start')
+
+  def stop(self):
+    self.send_message('/stop')
+
+  def set(self,key,value):
+    self.send_message('/set', [key, value])
+
+  def save(self,filename):
+    self.send_message('/save', [filename])
+
+  def play(self,note):
+    self.send_message('/play', [note + 40])
+
+client = BatchClient(args.ip, args.port)
 
 # 1: Guess test
 if (args.experiment in ['all', 'guess']):
+  print(f'[guess] Running experiment with model: ...')
+  for i in range(0,args.iterations):
+      client.play(i)
+      print(f'[guess] Iteration {i}')
+      # reset history
+      # /set buffer_size
+      # load (create function, cropping to buffer_size)
   pass
 
