77e6a31b36eacd6e1e045e26c4e97e57618e52da
ghalestrilo <ghalestrilo@gmail.com>
2021-05-10 22:33:56 -0300

    ⛏ finish dockerfile for rl_duet

---
+++ modules/rl_duet/ornette.py
@@ -0,0 +1,16 @@
+
+class OrnetteModule():
+    def __init__(self, host, checkpoint='rl_duet'):
+      pass
+
+    def generate(self, history=None, length_seconds=4):
+      return history[0]
+
+    def decode(self, token):
+      return None
+
+    def encode(self, message):
+      return None
+
+    def close(self):
+      return None
