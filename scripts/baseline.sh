75a1fd153b85ea17787cee9833ed3a65aef3046b
ghalestrilo <ghalestrilo@gmail.com>
2021-08-11 17:46:18 -0300

    ✨Implement Best Config Plot, implement baseline generation cell

--- scripts/baseline.sh
+++ scripts/baseline.sh
@@ -45,34 +45,62 @@ docker run
     --primer_melody="[]"
  
 
+###############################################################################################
 
-# PianorollRNN-NADE: Generate Samples
+# TODO: Crop dataset (via ornette) samples before feeding to models
 
-docker run
-  -v $HOME/.ornette/checkpoints:/checkpoints
-  -v $HOME/git/ornette/output:/output
+# Problem: Not monophonic melodies
+docker run \
+  -v $HOME/.ornette/checkpoints:/checkpoints \
+  -v $HOME/git/tg-server/output:/output \
+  -it ornette/melody_rnn \
+  performance_rnn_generate \
+    --config=attention_rnn \
+    --bundle_file=/checkpoints/melody_rnn/attention_rnn \
+    --output_dir=/output/baseline/melody_rnn \
+    --num_outputs=$number_of_samples \
+    --num_steps=256 \
+    --primer_midi=/output/dataset/sample-0.mid
+
+# OK
+docker run \
+  -v $HOME/.ornette/checkpoints:/checkpoints \
+  -v $HOME/git/tg-server/output:/output \
+  -it ornette/polyphony_rnn \
+  polyphony_rnn_generate \
+    --bundle_file=/checkpoints/polyphony_rnn/polyphony_rnn \
+    --output_dir=/output/baseline/polyphony_rnn \
+    --num_outputs=10 \
+    --num_steps=256 \
+    --primer_midi=/output/dataset/sample-0.mid \
+    --condition_on_primer=true \
+    --inject_primer_during_generation=false
+
+# OK
+docker run \
+  -v $HOME/.ornette/checkpoints:/checkpoints \
+  -v $HOME/git/tg-server/output:/output \
   -it ornette/pianoroll_rnn_nade \
   pianoroll_rnn_nade_generate \
     --config=rnn-nade_attn \
     --bundle_file=/checkpoints/pianoroll_rnn_nade/rnn-nade_attn \
+    --output_dir=/output/baseline/pianoroll_rnn_nade \
+    --num_outputs=4 \
+    --num_steps=256 \
+    --primer_midi=/output/dataset/sample-0.mid \
+
+# OK
+docker run \
+  -v $HOME/.ornette/checkpoints:/checkpoints \
+  -v $HOME/git/tg-server/output:/output \
+  -it ornette/performance_rnn \
+  performance_rnn_generate \
+    --config=performance_with_dynamics \
+    --bundle_file=/checkpoints/performance_rnn/performance_with_dynamics \
     --output_dir=/output/baseline/performance_rnn \
-    --num_outputs=$number_of_samples \
-    --num_steps=64 \
-    --primer_pitches="[67,64,60]"
+    --num_outputs=10 \
+    --num_steps=6400 \
+    --primer_midi=/output/dataset/sample-0.mid
 
 
-# PolyphonyRNN: Generate Samples
-
-docker run
-  -v $HOME/.ornette/checkpoints:/checkpoints
-  -v $HOME/git/ornette/output:/output
-  -it ornette/polyphony_rnn
-  polyphony_rnn_generate \
-    --bundle_file=${BUNDLE_PATH} \
-    --output_dir=/tmp/polyphony_rnn/generated \
-    --num_outputs=$number_of_samples \
-    --num_steps=128 \
-    --primer_pitches="[67,64,60]" \
-    --condition_on_primer=true \
-    --inject_primer_during_generation=false
-
+# PerformanceRNN: testar multiplicar quantidade de steps (total quantized steps + time de cada beat) por 100
