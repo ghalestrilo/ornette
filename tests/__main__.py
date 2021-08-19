048355e7d240bb7b10efdfce71442652c315112d
ghalestrilo <ghalestrilo@gmail.com>
2021-08-19 17:14:12 -0300

    ⛏ stub tests for song class

--- tests/__main__.py
+++ tests/__main__.py
@@ -7,18 +7,21 @@ import unittest
 import sys
 import os
 # sys.path.append(os.path.abspath(os.path.join(os.path.pardir, 'server')))
+sys.path.append(os.path.abspath(os.path.join('server')))
+# sys.path.append(os.path.abspath(os.path.join('server', 'tests')))
 # from server import filter_defs
 
-import test_filters
+
+# import test_filters
+from tests import test_song
 
 # initialize the test suite
 loader = unittest.TestLoader()
 suite  = unittest.TestSuite()
 
 # add tests to the test suite
-# suite.addTests(loader.loadTestsFromModule(player))
-# suite.addTests(loader.loadTestsFromModule(scenario))
-suite.addTests(loader.loadTestsFromModule(test_filters))
+# suite.addTests(loader.loadTestsFromModule(test_filters))
+suite.addTests(loader.loadTestsFromModule(test_song))
 
 # initialize a runner, pass it your suite and run it
 runner = unittest.TextTestRunner(verbosity=3)
--- tests/test_song.py
+++ tests/test_song.py
@@ -3,18 +3,44 @@ import sys
 from os import path
 
 # Load server folder
-sys.path.append(path.abspath(path.join(path.pardir, 'server')))
+sys.path.append(path.abspath(path.join('server')))
 
 from server.song import Song
+from server.host import Host
+from server.args import get_arg_parser
+
+# TODO: move to 
+test_args = get_arg_parser().parse_args(['--module','melody_rnn','--checkpoint','attention_rnn','--no-server','True','--no-module=True'])
+
 
 # python -m unittest tests.song
 
 class TestSong(unittest.TestCase):
+    # @classmethod
+    def setUp(self):
+        self.host = Host(test_args)
+        self.file = 'dataset/clean_mtd-orig/MTD0391_Bach_BWV0847-01.mid'
+        # self.host.set('log_level', 0)
 
-    # Mock tests
-    def buffer(self):
+    # @classmethod
+    def tearDown(self):
+        pass
+
+    def test_buffer(self):
       """ IF a buffer longer than the current song is requested
           It should return the entire song
       """
-      song = Song(self, self)
+      song = Song(self.host)
+      self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_crop_from_start(self):
+      """ A crop should remove exactly the requested number of bars from the start of the song
+      """
+      # song = Song(self.host, self.file)
+      self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_crop_from_end(self):
+      """ A crop should remove exactly the requested number of bars from the start of the song
+      """
+      # song = Song(self.host, self.file)
       self.assertEqual('foo'.upper(), 'FOO')
