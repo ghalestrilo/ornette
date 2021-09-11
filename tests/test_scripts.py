6a5007c4e44b551a90f5e9da8f07c94d00e9f760
ghalestrilo <ghalestrilo@gmail.com>
2021-09-10 23:47:07 -0300

    ⛏ make preprocessing tests pass

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -16,11 +16,11 @@ import random
 # Script Information
 script_output_bpm = 60
 desired_bars = sample_length # 16
-desired_seconds = desired_bars * (120/script_output_bpm) # 32
+desired_seconds = desired_bars * (60/script_output_bpm) # 32
 
 
 def runscript(script, cwd=os.path.curdir):
-  write(f'running: {" ".join(script)}')
+  # write(f'running: {" ".join(script)}')
   # result = subprocess.run(script, capture_output=True, text=True).stdout
   result = subprocess.run(script, capture_output=True,cwd=cwd)
   if result.stdout: print(result.stdout.decode('utf8'))
@@ -45,8 +45,10 @@ def preprocess(_in, _out, logfile=None, trim_start = -1):
 
     # Initialize tmp file
     # shutil.copy(_in,tmpfile)
-    runscript(prepscript(_in, tmpfile, 'stat'))
-    runscript(prepscript(tmpfile, tmpfile, 'tempo', str(120/bpm)))
+    # runscript(prepscript(_in, tmpfile, 'stat'))
+    # We could pre-trim the file here?
+    runscript(prepscript(_in, tmpfile, 'trim', str(0), '90')) # Cut 1:30 from the song
+    # runscript(prepscript(tmpfile, tmpfile, 'tempo', str(120/bpm)))
     _mid = mido.MidiFile(tmpfile)
     for i, track in enumerate(_mid.tracks[1:]):
       for msg in track[:40]:
@@ -55,28 +57,42 @@ def preprocess(_in, _out, logfile=None, trim_start = -1):
     _mid.save(tmpfile)
 
     # Seek for a contiguous part
+    search_threshold = min(mid.length, 100)
     best_length = 0
-    best_length_start = 0
-    while best_length < target_seconds:
-        trim_start += 1
+    best_start = 0
+    best_diff = abs(best_length - target_seconds)
+    delta = 0.5
+    # diff = abs(best_length - target_seconds)
+    # while best_length < target_seconds:
+    trim_start = max(trim_start,0)
+    while best_diff > delta:
         trim(trim_start)
         mid_out = mido.MidiFile(_out)
+        diff = abs(mid_out.length - target_seconds)
 
         # Detect longest contiguous segment
-        if mid_out.length > best_length:
+        if diff < best_diff:
           best_length = mid_out.length
-          best_length_start = trim_start
+          best_start = trim_start
         
-        if best_length >= target_seconds: break
+        if diff < delta: break
 
         # If all file was consumed
-        if trim_start + target_seconds > mid.length:
-            write(f'No contiguous {target_seconds}s segment in {_out}')
-            trim_start = best_length_start
+        
+        # if trim_start + mid_out.length > search_threshold:
+        if trim_start + mid_out.length > search_threshold:
+            # write(f'No contiguous {target_seconds}s segment in {_out}')
+            trim_start = best_start
             trim(trim_start)
-            print(f'needs {target_seconds - best_length} seconds')
-            runscript(prepscript(_out, _out, 'pad', '0', str(target_seconds - best_length)))
+
+            missing_seconds = target_seconds - best_length
+
+            if missing_seconds > 0:
+              # write(f'needs {missing_seconds} seconds')
+              runscript(prepscript(_out, _out, 'pad', '0', str(missing_seconds)))
             break
+
+        trim_start += 1
 
     # Merging must go at the end. 
     # Otherwise, it will merge data into the conductor and possibly mess up everything
@@ -86,9 +102,6 @@ def preprocess(_in, _out, logfile=None, trim_start = -1):
     if 'time_signature' not in map(lambda msg: msg.type, mid):
       mid.tracks[0].insert(1,mido.MetaMessage('time_signature',numerator=4,denominator=4,time=0))
       mid.save(_out)
-
-    # TEST
-    runscript(prepscript('-m', tmpfile, _out, 'trim', str(_start), str(target_seconds)))
 
 
 
